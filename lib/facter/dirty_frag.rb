28df57d9011dd9004e45aa9fe16aab9297c33ca6
Tony Green <tgreen@albatrossflavour.com>
2026-05-11 12:03:41 +1000

    feat: Implement structured dirty_frag custom fact with per-module loade…
    - lib/facter/dirty_frag.rb
    
    GSD-Task: S01/T02

---
+++ lib/facter/dirty_frag.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+Facter.add('dirty_frag') do
+  confine kernel: 'Linux'
+
+  setcode do
+    modules = %w[esp4 esp6 rxrpc]
+
+    begin
+      proc_modules_content = File.read('/proc/modules')
+      loaded_modules = proc_modules_content.each_line.map { |line| line.split.first }.compact
+    rescue StandardError => e
+      next { 'error' => "Failed to read /proc/modules: #{e.message}" }
+    end
+
+    result = {}
+
+    modules.each do |mod|
+      loaded = loaded_modules.include?(mod)
+
+      blacklisted = false
+      modprobe_dir = '/etc/modprobe.d'
+      if File.directory?(modprobe_dir)
+        begin
+          Dir.glob(File.join(modprobe_dir, '*')).each do |conf_file|
+            next unless File.file?(conf_file)
+
+            File.readlines(conf_file).each do |line|
+              if line.match?(/^\s*install\s+#{Regexp.escape(mod)}\s+\/bin\/(true|false)/)
+                blacklisted = true
+                break
+              end
+            end
+            break if blacklisted
+          end
+        rescue StandardError
+          # If we cannot read modprobe.d files, treat as not blacklisted
+        end
+      end
+
+      modinfo_result = Facter::Core::Execution.execute("modinfo #{mod}", on_fail: :failed)
+      available = modinfo_result != :failed
+
+      result[mod] = {
+        'loaded'      => loaded,
+        'blacklisted' => blacklisted,
+        'available'   => available,
+      }
+    end
+
+    result
+  end
+end
